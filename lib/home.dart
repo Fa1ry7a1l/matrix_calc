91260e9a8bed9567ef6b1905fd22ddcd169b9ad9
Андрей <andrew-sush@mail.ru>
2021-04-28 15:40:37 +0300

    я устал 2

--- lib/home.dart
+++ lib/home.dart
@@ -9,40 +9,134 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String a;
+  String b;
+  String l;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(40.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
                     "A",
                     style: TextStyle(fontSize: 20),
                   ),
                   new DropdownButton<String>(
-                    style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+                    style: TextStyle(
+                        fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
                     items: <String>['1', '2', '3', '4', '5', '6', '7']
                         .map((String value) {
                       return new DropdownMenuItem<String>(
                         value: value,
-                        child: new Text(value, style: TextStyle(fontSize: 20.0),),
+                        child: new Text(
+                          value,
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
                       );
                     }).toList(),
-                    onChanged: (_) {},
-                  )
+                    onChanged: (String s) {
+                      print(s);
+                      a = s;
+                      this.setState(() {});
+                    },
+                    hint: Text(
+                      a,
+                      style: TextStyle(
+                          fontSize: 20.0, color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  ),
+                  Text(
+                    "B",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  new DropdownButton<String>(
+                    style: TextStyle(
+                        fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+                    items: <String>['1', '2', '3', '4', '5', '6', '7']
+                        .map((String value) {
+                      return new DropdownMenuItem<String>(
+                        value: value,
+                        child: new Text(
+                          value,
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String s) {
+                      print(s);
+                      b = s;
+                      this.setState(() {});
+                    },
+                    hint: Text(
+                      b,
+                      style: TextStyle(
+                          fontSize: 20.0, color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  ),
+                  Text(
+                    "L",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  new DropdownButton<String>(
+                    style: TextStyle(
+                        fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+                    items: <String>['1', '2', '3', '4', '5', '6', '7']
+                        .map((String value) {
+                      return new DropdownMenuItem<String>(
+                        value: value,
+                        child: new Text(
+                          value,
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String s) {
+                      print(s);
+                      l = s;
+                      this.setState(() {});
+                    },
+                    hint: Text(
+                      l,
+                      style: TextStyle(
+                          fontSize: 20.0, color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  ),
+
                 ],
-              )
+              ),
+              MaterialButton(
+                onPressed: () => {},
+                color: Color.fromARGB(255, 159, 159, 237),
+                minWidth: 200.0,
+                child: Text("Создать матрицу"),
+              ), // Добавить функцию генераци
             ],
           ),
         ),
       ),
     );
+  }
+
+  _HomeState() {
+    a = "1";
+    b = "1";
+    l = "1";
   }
 }
