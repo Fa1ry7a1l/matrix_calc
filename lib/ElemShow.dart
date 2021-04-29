ec6adb63875a997937690710e71ccd62c2be399c
WiseCrucian <maxim.polyanskiy@mail.ru>
2021-04-29 23:38:30 +0300

    не устал

--- lib/ElemShow.dart
+++ lib/ElemShow.dart
@@ -1,24 +1,25 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'EnterMatrix.dart';
 
 class ElemShow extends StatelessWidget {
-
   List<List<int>> _matr = null;
   List<String> _strs = null;
-  ElemShow(List<List<int>> matrix, {List<String> strs})
-  {
-    _matr=matrix;
-    _strs=strs;
+  ElemShow(List<List<int>> matrix, {List<String> strs}) {
+    _matr = matrix;
+    _strs = strs;
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_strs==null){
-      return Text("AAAAAA");
+    print(_matr);
+    String str = "";
+    for (int i = 0; i < _matr.length; i++) {
+      for (int j = 0; j < _matr[i].length; j++) {
+        str += _matr[i][j].toString() + " ";
+      }
+      return Text(str);
     }
-    return Text("BBBBB");
   }
 }
