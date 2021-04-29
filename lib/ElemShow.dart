695f1d96c4c2c10222521203cdfb29dc38e63e72
Андрей <andrew-sush@mail.ru>
2021-04-29 22:57:05 +0300

    я устал 6

--- lib/ElemShow.dart
+++ lib/ElemShow.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +5,20 @@ import 'package:flutter/material.dart';
 import 'EnterMatrix.dart';
 
 class ElemShow extends StatelessWidget {
+
+  List<List<int>> _matr = null;
+  List<String> _strs = null;
+  ElemShow(List<List<int>> matrix, {List<String> strs})
+  {
+    _matr=matrix;
+    _strs=strs;
+  }
+
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    if (_strs==null){
+      return Text("AAAAAA");
+    }
+    return Text("BBBBB");
+  }
 }
--- lib/ViewMatrix.dart
+++ lib/ViewMatrix.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'EnterMatrix.dart';
+import 'package:matrix_calc/ElemShow.dart';
 
 class Show extends StatelessWidget {
   List<List<List<int>>> _matrList;
   List<List<String>> _stringList;
+  List<Widget> _list = [];
 
   Show(List<List<List<int>>> matrList, List<List<String>> stringList) {
     this._matrList = matrList;
     this._stringList = stringList;
+    _list = ShowList(_matrList, _stringList);
+    print(_list);
   }
 
   @override
@@ -19,34 +21,25 @@ class Show extends StatelessWidget {
         title: Text("Решение"),
       ),
       body: Center(
-        child: 
-          ListView(
-          padding: const EdgeInsets.all(8),
-          children: ShowList(_matrList),
-           
+        child: ListView(
+          children: _list,
         ),
-        ),
-      );
+      ),
     );
   }
 
+  List<Widget> ShowList(
+      List<List<List<int>>> matrList, List<List<String>> strList) {
+    List<Widget> list = [];
+    //if (matrList.length - strList.length < 3) throw new Exception("Пидоры");
 
-  List<Widget> ShowList(List<List<List<int>>> matrList)
-  {
-    ListView mylist =  ListView.builder(
-      itemBuilder: (BuildContext ctxt, int index){
-        return new Text(matrList.first.first.first.toString());
-      }
-    
-      );
-      //return mylist;
+    for (int i = 0; i < strList.length; i++) {
+      list.add(new ElemShow(_matrList[i], strs: _stringList[i]));
+    }
+    for (int i = strList.length; i < matrList.length; i++) {
+      list.add(new ElemShow(_matrList[i]));
+    }
 
+    return list;
   }
-
-
-  
-
-
-
-
 }
